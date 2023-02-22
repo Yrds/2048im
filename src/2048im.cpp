da9a14c79160e44a7fa08553d7f78cdd0dfc299a
Yuri Santos <yrds96@protonmail.com>
2023-02-21 21:21:46 -0300

    nightly commit

--- src/2048im.cpp
+++ src/2048im.cpp
@@ -13,42 +13,77 @@ constexpr auto cellSize = 50;
 constexpr Vector2 windowSize = {800, 450};
 constexpr Vector2 gridPosition = {100, 100};
 
-auto grid = std::array<std::array<int, gridSize>, gridSize> {{
+using Grid =std::array<std::array<int, gridSize>, gridSize>;
+
+Grid grid = std::array<std::array<int, gridSize>, gridSize> {{
     {2, 2, 2, 2},
     {0, 4, 0, 2},
     {0, 2, 0, 2},
     {0, 2, 4, 8}
 }};
 
-void frame() {
+void moveHorizontal(Grid& grid, Vector2 direction) {
+  const auto columnStart = direction.x == -1 ? 0 : gridSize;
+  const auto columnEnd = direction.x == -1 ? gridSize - 1 : 0;
+  const auto columnDirection = direction.x == -1 ? 1 : -1;
 
+  for (auto line = 0; line != gridSize; line ++) {
+    for (auto column = columnStart; column != columnEnd; column += columnDirection) {
+      for (auto cell = column + columnDirection; cell != columnEnd + columnDirection; cell += columnDirection) {
+        if (grid[line][column] == grid[line][cell]) {
+          grid[line][column] = grid[line][cell] * 2;
+          grid[line][cell] = 0;
+        } else if (grid[line][column] == 0 && grid[line][cell]) {
+          grid[line][column] = grid[line][cell];
+          grid[line][cell] = 0;
+        }
+      }
+    }
+  }
+}
+
+void moveVertical(Grid& grid, Vector2 direction) {
+  const auto columnStart = direction.x == -1 ? 1 : gridSize - 1;
+  const auto columnEnd = direction.x == -1 ? gridSize - 1 : 1;
+  const auto columnDirection = direction.x == -1 ? 1 : -1;
+
+  for (auto line = 0; line != gridSize; line ++) {
+    for (auto column = columnStart; column != columnEnd; column += columnDirection) {
+      for (auto cell = column + columnDirection; cell != columnEnd; cell += columnDirection) {
+        if (grid[line][column] == grid[line][cell]) {
+          grid[line][column] = grid[line][cell] * 2;
+          grid[line][cell] = 0;
+        } else if (grid[line][column] == 0 && grid[line][cell]) {
+          grid[line][column] = grid[line][cell];
+          grid[line][cell] = 0;
+        }
+      }
+    }
+  }
+}
+
+void moveBlocks(Grid &grid, Vector2 direction) {
+  if(direction.x) {
+    moveHorizontal(grid, direction);
+  } else {
+  }
+}
+
+void frame() {
     Vector2 direction = {0, 0};
 
     if(IsKeyPressed(KEY_LEFT)) {
-        direction = {0, -1};
-    } else if(IsKeyPressed(KEY_RIGHT)) {
-        direction = {gridSize, -1};
-    } else if(IsKeyPressed(KEY_UP)) {
         direction = {-1, 0};
+    } else if(IsKeyPressed(KEY_RIGHT)) {
+        direction = {1, 0};
+    } else if(IsKeyPressed(KEY_UP)) {
+        direction = {0, -1};
     } else if (IsKeyPressed(KEY_DOWN)) {
-        direction = {-1, gridSize};
+        direction = {0, 1};
     }
 
     if(!(direction.x == 0 && direction.y == 0)) {
-
-        for(auto line = static_cast<int>(direction.x); line != gridSize; line++) {
-            for(auto column = 0; column < gridSize - 1; column ++) {
-                for(auto cell = column + 1; cell < gridSize; cell++) {
-                    if(grid[line][column] == grid[line][cell]) {
-                        grid[line][column] = grid[line][cell] * 2;
-                        grid[line][cell] = 0;
-                    } else if(grid[line][column] == 0 && grid[line][cell]) {
-                        grid[line][column] = grid[line][cell];
-                        grid[line][cell] = 0;
-                    }
-                }
-            }
-        }
+        moveBlocks(grid, direction);
     }
 
     BeginDrawing();
